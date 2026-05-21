dc22f35df2815743e131313075b3b7e379dad745
Shrishobana <47035552+shrishobana-thiyagarajan@users.noreply.github.com>
2026-05-21 10:05:15 +0200

    New commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Azure 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
