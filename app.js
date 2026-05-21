d6746dd84c72eb28fa08ea2c8e3f62f08ba25cf3
Shrishobana <47035552+shrishobana-thiyagarajan@users.noreply.github.com>
2026-05-21 10:16:57 +0200

    message added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from Azure 🚀");
+  res.send("Hello Azure CI/CD ✅");
+  res.send("Now deployment works ✅");
+
 });
 
 app.listen(3000, () => {
